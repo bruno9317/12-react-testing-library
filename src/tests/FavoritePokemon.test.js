97c5a359bb1aed0c3268aa69e07ab9f31da6de66
bruno <brunobaptista9317@gmail.com>
2022-11-22 20:27:28 -0300

    atualizando requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -15,6 +15,7 @@ test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a p
 test('Teste se são exibidos todos os cards de Pokémon favoritados.', () => {
   addPokemonToFavorites(25);
   addPokemonToFavorites(143);
+  // addPokemonToFavorites(143);
   renderWithRouter(<App />);
   localStorage.setItem('favoritePokemonIds', 25);
   const favoriteLink = screen.getByRole('link', { name: 'Favorite Pokémon' });
